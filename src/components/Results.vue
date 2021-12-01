b52196a5fb2a499aa8289aab2cae486f40cdadfb
Patrick Kim <patrick.yrkim@gmail.com>
2021-12-01 00:08:42 -0800

    Add ranks for reation time

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,5 +1,6 @@
 <template>
     <p>Reaction Time: {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
@@ -7,9 +8,26 @@ export default {
     props: [
         'score'
     ],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'ninja fingers'
+        } else if (this.score < 400) {
+            this.rank = 'rapid reflexes'
+        } else {
+            this.rank = 'slowpoke'
+        }
+    }
 }
 </script>
 
 <style>
-
+    .rank {
+        color: tomato;
+        font-weight: bold;
+    }
 </style>
