d82e9929f47e55e264ddaa9764baae94c8c9b809
Patrick Kim <patrick.yrkim@gmail.com>
2021-11-30 23:34:08 -0800

    Add Block component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <h1>Reaction Timer</h1>
-  <button @click="start">Start</button>
+  <button @click="start" :disabled="isPlaying">Start</button>
+  <Block v-if="isPlaying" v-bind:delay="delay" /> <!-- or just : to bind -->
 </template>
 
 <script>
+import Block from './components/Block.vue'
+
 export default {
   name: 'App',
-  components: {  },
+  components: { Block },
   data() {
     return {
       isPlaying: false,
